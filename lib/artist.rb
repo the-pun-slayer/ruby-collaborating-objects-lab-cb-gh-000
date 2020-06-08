0e9ffe5cd1e4ba9727da7ff7a97addcef1f199cc
PUNEET MANDIL <puneetmandil.mec18@itbhu.ac.in>
2020-06-08 17:53:18 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,24 +1,37 @@
 class Artist
-  @@all=[]
-  attr_accessor :name
-  
+  @@all = []
+  attr_accessor :name, :songs
+
   def initialize(name)
-    @name=name
-    @@all<<self
+    @name = name
+    save
   end
-  
-  def add_song(song)
-    song.name= self
-  end
-  
-  def songs 
-    Song.all.select do {|s| s.artist==self}
-  end
-  
+
   def self.all
-    @@all
+    @@all    
   end
-  
+
+  def add_song(song)
+    song.artist = self
+  end
+
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
   def self.find_or_create_by_name(name)
-    if self.all.
-    
+    self.find(name) ? self.find(name) : self.new(name)
+  end
+
+  def self.find(name)
+    self.all.find {|artist| artist.name == name }
+  end
+
+  def save
+    @@all << self
+  end
+
+  def print_songs
+    songs.each {|song| puts song.name}
+  end
+end
